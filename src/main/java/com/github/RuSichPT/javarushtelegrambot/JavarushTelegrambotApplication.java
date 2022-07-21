b3d92759b581f106ea8330d694a0b1dad2307e35
pavel.tokarev <pavel.tokarev@mail.ntc51>
2022-07-21 14:44:36 +0300

    JRTB-0: added SpringBoot skeleton to this project

---
+++ src/main/java/com/github/RuSichPT/javarushtelegrambot/JavarushTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.javarushtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/RuSichPT/javarushtelegrambot/JavarushTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.javarushtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
